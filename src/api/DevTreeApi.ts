3ab7235890ba98f5495eb25b75f9d9a284d905f4
Miguel <mmirandasanchez16@gmail.com>
2025-03-02 09:18:49 -0300

    message

---
+++ src/api/DevTreeApi.ts
@@ -0,0 +1,40 @@
+import { isAxiosError } from "axios";
+import api from "../config/axios";
+import { ProfileForm, User } from "../types";
+
+export const getUser = async () => {
+  try {
+    const { data } = await api<User>("/user");
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+};
+
+export const updateProfile = async (formData: User) => {
+  try {
+    const { data } = await api.patch<string>("/user", formData);
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error);
+    }
+  }
+};
+
+export const uploadImage = async (file: File) => {
+  let formData = new FormData();
+  formData.append("file", file);
+  try {
+    const {
+      data: { image },
+    }: { data: { image: string } } = await api.post("/user/image", formData);
+    return image;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.message);
+    }
+  }
+};
